8a3620e28d08d6a921be3e0341b8d4960e753812
Prachi Prakash <prakash@ACA88149.ipt.aol.com>
2016-07-27 10:15:18 +0530

    thread tutorial

---
+++ TACodes/src/threads/FirstThread.java
@@ -0,0 +1,17 @@
+package threads;
+class PrinterThread implements Runnable
+{
+	public void run()
+	{
+		System.out.println("Hello World");
+	}
+}
+public class FirstThread 
+{
+	public static void main(String args[])
+	{
+		Thread t = new Thread(new PrinterThread());
+		t.start();
+		
+	}
+}
